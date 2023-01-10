7bf18379c3534db7ca65b97584f76201b83b0ca9
Bohdan <bohdansalai@gmail.com>
2023-01-10 17:00:16 +0100

    implemented authentication

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -12,6 +12,9 @@ import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import javax.annotation.security.PermitAll;
+
+@PermitAll
 @Route(value = "", layout = MainLayout.class)
 @PageTitle("Contacts | Vaadin CRM")
 
--- src/main/java/com/example/application/views/list/MainLayout.java
+++ src/main/java/com/example/application/views/list/MainLayout.java
@@ -1,7 +1,9 @@
 package com.example.application.views.list;
 
+import com.example.application.security.SecurityService;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -10,8 +12,10 @@ import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.RouterLink;
 
 public class MainLayout extends AppLayout {
+    private final SecurityService securityService;
 
-    public MainLayout(){
+    public MainLayout(SecurityService securityService){
+        this.securityService = securityService;
         createHeader();
         createDrawer();
     }
@@ -20,12 +24,15 @@ public class MainLayout extends AppLayout {
         H1 logo = new H1("Vaadin CRM");
         logo.addClassNames("text-1", "m-m");
 
+        Button logout = new Button("Log out", e -> securityService.logout());
+
         HorizontalLayout header = new HorizontalLayout(
                 new DrawerToggle(),
-                logo
+                logo, logout
         );
 
         header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        header.expand(logo);
         header.setWidth("100%");
         header.addClassNames("py-0", "px-m");
 
